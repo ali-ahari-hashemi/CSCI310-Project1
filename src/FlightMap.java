8911da74e55adfd0296145bd76ecc991eecb3a73
Ali Hashemi <alihashemi@Alis-MacBook-Pro.local>
2018-09-15 21:05:50 -0700

    added FlightMap and SearchMap class files

---
+++ src/FlightMap.java
@@ -0,0 +1,7 @@
+public class FlightMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/SearchMap.java
@@ -0,0 +1,6 @@
+public class SearchMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
