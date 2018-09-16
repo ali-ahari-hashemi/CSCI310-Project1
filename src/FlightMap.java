f848e897b1c891c2f5c97caefbd603ddfa6613de
Ali Hashemi <alihashemi@Alis-MacBook-Pro.local>
2018-09-15 23:44:17 -0700

    FlightMap class complete

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -1,7 +1,126 @@
+import java.util.*;
+
 public class FlightMap {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public FlightMap(char originCity) {
+        this.originCity = originCity;
+        this.cities = new HashSet<Character>();
+        cities.add(originCity);
+        this.flights = new HashMap<Character, List<Flight>>();
+        this.flights.put(originCity, new ArrayList<Flight>());
+    }
+
+
+    public void addFlight(char departure, char destination, int cost) {
+        cities.add(departure);
+        cities.add(destination);
+
+        Flight flight = new Flight(departure, destination, cost);
+        List<Flight> flights = this.flights.get(departure);
+
+        if (flights != null) {
+           flights.add(flight);
+        }
+        else {
+            flights = new ArrayList<Flight>();
+            flights.add(flight);
+            this.flights.put(departure, flights);
+        }
+    }
+
+    public String getRouteInfo() {
+        String allRouteInfo = "Destination \t Flight Route from " + Character.toString(this.originCity) + " \t Total Cost \n";
+        Iterator<Character> it = this.cities.iterator();
+        while(it.hasNext()) {
+            char destination = it.next();
+            if (destination != this.originCity) {
+                String formattedRouteInfo = getFlightRouteFormattedString(destination);
+                if (formattedRouteInfo != null) {
+                    allRouteInfo += formattedRouteInfo + "\n";
+                }
+            }
+        }
+        return allRouteInfo;
+    }
+
+
+    private String getFlightRouteFormattedString(char destination) {
+        List<Flight> flightRoute = getFlightRouteToDestination(destination);
+        if (flightRoute == null) { return null; }
+
+        String formattedOutput = Character.toString(destination) + "\t";
+        String route = Character.toString(this.originCity);
+        int totalCost = 0;
+
+        for (int i = 0; i < flightRoute.size(); i++) {
+            Flight currentFlight = flightRoute.get(i);
+            route += ", " + currentFlight.destination;
+            totalCost += currentFlight.cost;
+        }
+        formattedOutput += route + "\t" + "$" + Integer.toString(totalCost);
+
+        return formattedOutput;
+    }
+
+
+    private List<Flight> getFlightRouteToDestination(char destination) {
+       List<Flight> flightRoute = helper(this.originCity, destination, new ArrayList<Flight>(), new HashSet<Character>());
+       return flightRoute;
+    }
+
+
+    private List<Flight> helper(char currentCity, char destination, List<Flight> flightsVisited, HashSet<Character> citiesVisited) {
+
+        if (currentCity == destination) {
+            return flightsVisited;
+        }
+
+        List<Flight> flightsFromCurrentCity = this.flights.get(currentCity);
+        if (flightsFromCurrentCity != null) {
+            for (int i = 0; i < flightsFromCurrentCity.size(); i++) {
+                if (!citiesVisited.contains(currentCity)) {
+                    Flight currentFlight = flightsFromCurrentCity.get(i);
+                    flightsVisited.add(currentFlight);
+                    citiesVisited.add(currentCity);
+                    List<Flight> flightRoute = helper(currentFlight.destination, destination, flightsVisited, citiesVisited);
+                    if (flightRoute != null) {
+                        return flightRoute;
+                    }
+                    flightsVisited.remove(currentFlight);
+                    citiesVisited.remove(currentCity);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void printFlights() {
+         Iterator it = this.flights.entrySet().iterator();
+         while(it.hasNext()) {
+             Map.Entry<Character, List<Flight>> pair = (Map.Entry)it.next();
+             System.out.println("Departure: " + pair.getKey());
+             List<Flight> flights = pair.getValue();
+             for (int i = 0; i < flights.size(); i++) {
+                 System.out.println("    Destination: " + flights.get(i).destination + " Cost: " + flights.get(i).cost);
+             }
+         }
+    }
+
+    private char originCity;
+    private Set<Character> cities;
+    private Map<Character, List<Flight>> flights;
+
+    private class Flight {
+        public Flight(char departure, char destination, int cost) {
+            this.departure = departure;
+            this.destination = destination;
+            this.cost = cost;
+        }
+        public char departure;
+        public char destination;
+        public int cost;
     }
 
 }
--- src/SearchMap.java
+++ src/SearchMap.java
@@ -1,6 +1,22 @@
 public class SearchMap {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        FlightMap flightMap = new FlightMap('P');
+        flightMap.addFlight('P', 'W', 200);
+        flightMap.addFlight('P', 'R', 300);
+        flightMap.addFlight('R', 'X', 200);
+        flightMap.addFlight('Q', 'X', 375);
+        flightMap.addFlight('W', 'S', 250);
+        flightMap.addFlight('S', 'T', 300);
+        flightMap.addFlight('T', 'W', 350);
+        flightMap.addFlight('W', 'Y', 500);
+        flightMap.addFlight('Y', 'Z', 450);
+        flightMap.addFlight('Y', 'R', 600);
+
+        String result = flightMap.getRouteInfo();
+        System.out.println(result);
+
     }
+
 }
